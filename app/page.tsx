eaa05ac5c019a0e03fee7667076fc723605f9998
NATION <chiburomanation6@gmail.com>
2026-07-27 13:34:32 +0100

    feat: promote Bootcamp Spotlight to position #2 on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { Chapter1Arrival } from "@/components/home/chapters/Chapter1Arrival";
+import { Chapter7Training } from "@/components/home/chapters/Chapter7Training";
 import { Chapter3WhoWeAre } from "@/components/home/chapters/Chapter3WhoWeAre";
 import { Chapter4WhatWeBuild } from "@/components/home/chapters/Chapter4WhatWeBuild";
 import { Chapter5HowWeSolve } from "@/components/home/chapters/Chapter5HowWeSolve";
-import { Chapter7Training } from "@/components/home/chapters/Chapter7Training";
 import { Chapter2Future } from "@/components/home/chapters/Chapter2Future";
 import { Chapter6Industries } from "@/components/home/chapters/Chapter6Industries";
 import { Chapter8SocialProof } from "@/components/home/chapters/Chapter8SocialProof";
@@ -14,17 +14,17 @@ export default function HomePage() {
       {/* 1. Hero & Statement of Excellence */}
       <Chapter1Arrival />
 
-      {/* 2. Mission, Vision, Values & Identity */}
+      {/* 2. Promoted Bootcamp Spotlight & Training Registration */}
+      <Chapter7Training />
+
+      {/* 3. Mission, Vision, Values & Identity */}
       <Chapter3WhoWeAre />
 
-      {/* 3. Core Enterprise Capabilities */}
+      {/* 4. Core Enterprise Capabilities */}
       <Chapter4WhatWeBuild />
 
-      {/* 4. Engineering Workflow & Problem-Solving Process */}
+      {/* 5. Engineering Workflow & Problem-Solving Process */}
       <Chapter5HowWeSolve />
-
-      {/* 5. Promoted Bootcamp Spotlight & Training Registration */}
-      <Chapter7Training />
 
       {/* 6. Engineering Tech Stack & Tools Arsenal */}
       <Chapter2Future />
@@ -40,4 +40,5 @@ export default function HomePage() {
     </>
   );
 }
+
 
